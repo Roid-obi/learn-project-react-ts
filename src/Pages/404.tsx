04e0f0708c318a10e3def5c1de2be3d058c5c66c
Roid-obi <contohdiamazon2@gmail.com>
2023-08-01 20:37:59 +0700

    Create ErrorPage

--- src/Pages/404.tsx
+++ src/Pages/404.tsx
@@ -1,15 +1,16 @@
-import { useRouteError } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const ErrorPage = () => {
-    const error = useRouteError(); // Panggil useRouteError sebagai fungsi untuk mendapatkan nilai error.
+  const location = useLocation();
+  const error = location.state && location.state.error;
 
-    return (
-        <div className="flex justify-center min-h-screen items-center flex-col gap-y-5">
-            <h1 className="text-3xl font-bold">Oops!</h1>
-            <p className="text-xl">Sorry, an unexpected error has occured</p>
-            <p>{error?.statusText || error?.message}💀</p> {/* Gunakan optional chaining (?) untuk mengakses statusText dan message */}
-        </div>
-    );
-}
+  return (
+    <div className="flex justify-center min-h-screen items-center flex-col gap-y-5">
+      <h1 className="text-3xl font-bold">Oops!</h1>
+      <p className="text-xl">Sorry, an unexpected error has occurred</p>
+      <p>{error?.statusText || error?.message} 💀</p>
+    </div>
+  );
+};
 
 export default ErrorPage;
